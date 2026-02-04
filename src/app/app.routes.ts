176400b8204870f34cdbd3fb10c40127022a66bf
dimitrije-savatic <dimitrije.savatic@gmail.com>
2026-02-04 18:49:58 +0100

    create app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./features/home/home').then(m => m.Home) },
+    { path: 'about', loadComponent: () => import('./features/about/about').then(m => m.About) },
+    { path: 'contact', loadComponent: () => import('./features/contact/contact').then(m => m.Contact) },
+    { path: 'products', loadComponent: () => import('./features/projects/projects').then(m => m.Projects) },
+    { path: '**', redirectTo: '' }
+];
